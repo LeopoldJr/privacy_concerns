c52476a294d87ac59c4781df25467b77485acb91
Aaron <anwoolf@clemson.edu>
2022-03-09 15:56:18 -0500

    Homepage now redirects to Settings page. Adjusted size of save button. Centered Settings header

--- src/pages/Navigation.js
+++ src/pages/Navigation.js
@@ -3,11 +3,11 @@ import Confirmation from "./Confirmation";
 import Questionaire from "./Questionaire";
 import { render } from "react-dom";
 import {
-  HashRouter,
   Routes,
   Route,
   Link,
-  Outlet
+  Outlet,
+  Navigate
 } from "react-router-dom";
 import useInputState from "../hooks/useInputState";
 import useToggleState from "../hooks/useToggleState";
@@ -17,7 +17,7 @@ import "../styling/Navigation.css";
 
 export default function Navigation(){
 const [data, setData] = useInputState(20);
-const [data2, toggleData2, reset2] = useToggleState(false);
+const [data2, toggleData2] = useToggleState(false);
 const [data3, toggleData3, reset3] = useToggleState(false);
 const [data4, toggleData4, reset4] = useToggleState(false);
 const [data5, toggleData5, reset5] = useToggleState(false);
@@ -32,6 +32,10 @@ const handleInputChange5 = (e) => { reset3(); reset4(); toggleData5();}
         <div>
             <h1><Link className="title-settings"to="/Settings">Settings</Link></h1>
             <Routes>
+            <Route
+                path="/"
+                element={<Navigate to="/Settings" />}
+            />
                 <Route path="/Settings" 
                 element={<Questionaire
                     handleInputChange3={handleInputChange3} handleInputChange4={handleInputChange4} handleInputChange5={handleInputChange5}
